d3e923d47988e1ef29f73e4b7d345df3ee39f4f7
= <=>
2019-01-17 19:58:52 +0200

    l

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def home_page():
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
 
---
+++ templates/button.js
@@ -0,0 +1,10 @@
+ 
+
+ document.getElementById("myButton").onclick = function () {
+        location.href="{{url_for('log_in')}}";
+    };
+
+ 
+ document.getElementById("sign_up_button").onclick = function () {
+        location.href='sign_up.html';
+    };
